3fc5c9504fe3a9ef3a61ae6cfe16a1d97d3f58b6
Gdeon1337 <gdeom1337@gmail.com>
2019-12-03 21:23:27 +0400

    deploy heroky

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :check_inn, CheckInnWeb.Endpoint,
-  url: [host: "morning-gorge-70943.herokuapp.com", port: 80],
+  url: [host: "shielded-savannah-57648.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
